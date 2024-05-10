27e157e2848fc5d2348e038061f699b3608270aa
Ian Zhang <20307110123@fudan.edu.cn>
2024-05-11 01:18:12 +0800

    apc

--- scnn/bipolar_functions/operations.py
+++ scnn/bipolar_functions/operations.py
@@ -8,10 +8,10 @@ def mux_selector(inputs):
     random_idx = torch.randint(low=0, high=no_channels, size=(seq_len,))
     return inputs[random_idx, torch.arange(seq_len)]
 
-def mul(x: torch.tensor, y: torch.tensor) -> torch.Any:
+def mul(x: torch.tensor, y: torch.tensor) -> torch.tensor:
     return torch.logical_not(torch.logical_xor(x,y))
 
-def scaled_add(x: torch.tensor, y: torch.tensor) -> torch.Any:
+def scaled_add(x: torch.tensor, y: torch.tensor) -> torch.tensor:
     x, y = torch.broadcast_tensors(x, y)
 
     select_x = F2S(seq_len=x.shape[-1], mode='unipolar').generate(torch.full(size=x.shape[:-1], fill_value=0.5))
@@ -24,11 +24,11 @@ def scaled_add(x: torch.tensor, y: torch.tensor) -> torch.Any:
 def mean(*inputs):
     return mux_selector(torch.stack(inputs, dim=0))
 
-def scaled_dot(x: torch.tensor, y: torch.tensor) -> torch.Any:
+def scaled_dot(x: torch.tensor, y: torch.tensor) -> torch.tensor:
     return mux_selector(mul(x,y))
 
 
-def scaled_matmul(x: torch.tensor, y: torch.tensor) -> torch.Any:
+def scaled_matmul(x: torch.tensor, y: torch.tensor) -> torch.tensor:
     x_shape, y_shape = x.shape, y.shape
     seq_len = x_shape[-1]
     assert seq_len == y_shape[-1] and x_shape[-2] == y_shape[0]
@@ -46,44 +46,60 @@ def scaled_matmul(x: torch.tensor, y: torch.tensor) -> torch.Any:
     return out
 
 
-def p_scaled_matmul(x: torch.tensor, y: torch.tensor) -> torch.Any:
-    x_shape, y_shape = x.shape, y.shape
-    seq_len = x_shape[-1]
-    assert seq_len == y_shape[-1] and x_shape[-2] == y_shape[0]
-    x_reshape = x.view(-1,x_shape[-2],seq_len)
-    y_reshape = y.view(y_shape[0], -1, seq_len)
+
+#tanh(xN/2)
+def tanh_fsm(x: torch.tensor, N) -> torch.tensor:
+    pass
+
+def counter(x):
+    return torch.sum(x, dim=-2, dtype=torch.int16)
+
+class ap_counter():
+    def __init__(self, bit=4, num_au_layers=1):
+        assert bit >= 2
+        self.num_in_channels = int(2**bit)
+        self.num_au_layers = num_au_layers
+
+
+    def approx_unit(self, x: torch.tensor) -> torch.tensor:
+        seq_len = x.shape[-1]
+        assert x.shape[-2] <= self.num_in_channels
+        if x.shape[-2] < self.num_in_channels:
+            x = torch.cat([x, torch.zeros(x.shape[:-2]+(self.num_in_channels-x.shape[-2],seq_len,), dtype=torch.bool)],dim=-2)
+
+        num_units = int(self.num_in_channels/4)
+
+        grp1,grp2,grp3,grp4 = x[...,:num_units,:], x[...,num_units: 2*num_units,:], x[...,2*num_units: 3*num_units,:], x[...,3*num_units: 4*num_units,:]
+        
+        out = torch.concat([torch.logical_and(grp3,grp4), torch.logical_or(grp1,grp2)], dim=-2)
+        
+        return out
     
-    out = torch.zeros(x_reshape.shape[0], y_reshape.shape[1],seq_len)
-    out_shape = x.shape[:-2] + y.shape[1:-1] + (seq_len,)
-
-    procs = []
-    queue = multiprocessing.Queue()
-
-    def helper(row_idx, x_slice, queue):
-        try:
-            loc_res = []
-            for j, y_slice in enumerate(y_reshape.transpose(0,1)):
-                print(f'doing row no.{row_idx} column no.{j}')
-                print(x_slice)
-                loc_res.append(scaled_dot(x_slice, y_slice))
-                print(f'done row no.{row_idx} column no.{j}')
-            tpl = (row_idx,loc_res)
-            print(f'done row no.{row_idx}')
-            queue.put(tpl)
-        except Exception as e:
-            print(e)
-
-    for row_idx, x_slice in enumerate(x_reshape):
-        proc = multiprocessing.Process(target=helper,args=(row_idx, x_slice, queue))
-        procs.append(proc)
-        proc.start()
-
-    for proc in procs:
-        proc.join()
+    def count(self, x):
+        return torch.sum(x, dim=-2, dtype=torch.int16)
     
-    for _ in range(x_reshape.shape[0]):
-        row_idx, loc_res = queue.get()
-        out[row_idx] = torch.stack(loc_res)
+    def __call__(self, x):
+        for i in range(self.num_au_layers):
+            x = self.approx_unit(x)
+        return 2**self.num_au_layers*self.count(x)
 
-    out = out.view(out_shape)
+#tanh(1/s * sum(inputs))
+def neuron_c(inputs, r: int):
+    num_in, seq_len = inputs.shape[-2], inputs.shape[-1]
+    s_max = r-1
+    s_half = (r-1)/2
+
+    s = torch.full(inputs.shape[:-2], s_half, dtype=torch.int16)
+    v = counter(inputs)*2 - num_in
+
+    out = torch.zeros(inputs.shape[:-2]+(seq_len,), dtype=torch.bool)
+
+    for i in range(seq_len):
+        s = s + v.select(dim=-1, index=i)
+        overflow_idx = s > s_max
+        underflow_idx = s < 0
+        s[overflow_idx] = s_max
+        s[underflow_idx] = 0
+        
+        out[..., i] = s > s_half
     return out
